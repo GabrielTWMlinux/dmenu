61eaa323f72c72787b7aadfacdf2b42d55efc87c
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-10-02 01:53:52 -0300

    Add files via upload

---
+++ themes/tokyo.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#16161E" },
+	[SchemeSel] = { "#ffffff", "#343b58" },
+	[SchemeOut] = { "#000000", "#66887c" },
+};
